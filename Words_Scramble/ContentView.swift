9d7857dc430138d27499a479c6b42e8f9d2be6ee
Shahid Arshad <shahid.arshad@devsinc.com>
2023-01-30 15:56:13 +0500

    Adding a list of words

---
+++ Words_Scramble/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Words_Scramble
+//
+//  Created by Dev on 30/01/2023.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        // extra validations
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Words_Scramble/Words_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Words_ScrambleApp.swift
+//  Words_Scramble
+//
+//  Created by Dev on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Words_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
